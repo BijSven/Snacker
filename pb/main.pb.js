102f5f802df5cb01aa9b715893c09b7cccc72e7e
Sven <sven-sve@proton.me>
2024-02-21 11:07:59 +0100

    update main.pb.js > Added validation to /log/:token

--- pb/main.pb.js
+++ pb/main.pb.js
@@ -27,17 +27,20 @@ routerAdd("POST", "/log/:token", (c) => {
 
     let token = c.pathParam("token")
 
-    const record = $app.dao().findRecordById("tokens", token);
+    const Token = $app.dao().findRecordById("tokens", token);
     const collection = $app.dao().findCollectionByNameOrId("logs");
+    const record = new Record(collection);
+    const form = new RecordUpsertForm($app, record);
 
-    $app.dao().saveRecord(
-        new Record(collection, {
-            "icon": body.icon,
-            "data": body.title,
-            "source": body.source,
-            "channel": record.get('channel'),
-        })
-    );
+    form.loadData({
+        "icon": body.icon,
+        "data": body.title,
+        "source": body.source,
+        "channel": Token.get('channel'),
+    })
+
+    form.submit();
+    
 
     $app.logger().info(
         `POST /log/${token}`,
