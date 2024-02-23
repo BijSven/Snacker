7b35bbe94e34b26c305a3643d884efc8674729b4
Sven <sven-sve@proton.me>
2024-02-23 07:45:45 +0100

    update activityShow.svelte > Little patch for responsive

--- src/lib/components/activity/activityShow.svelte
+++ src/lib/components/activity/activityShow.svelte
@@ -17,14 +17,14 @@
 </script>
 
 
-<content class="flex w-[40%] min-w-[40%] gap-4 rounded-sm p-5 bg-stone-200 dark:bg-stone-800">
+<content class="flex w-[40%] overflow-x-hidden min-w-[40%] gap-4 rounded-sm p-5 bg-stone-200 dark:bg-stone-800">
     <h1 class="size-12 min-h-12 min-w-12 dark:bg-stone-700 bg-stone-300 rounded-sm flex justify-center items-center text-2xl select-none whitespace-nowrap">{data.icon}</h1>
     <div>
-        <h1 class="text-xl text-left">{data.data}</h1>
+        <h1 class="text-xl text-left w-[95%]">{data.data}</h1>
         <div class="flex gap-2 select-none text-muted-foreground w-[100%] overflow-hidden">
-            <p class="w-[40%] overflow-hidden text-ellipsis whitespace-nowrap">{data.source}</p>
+            <p class="w-32 overflow-hidden text-ellipsis whitespace-nowrap">{data.source}</p>
             <p>•</p>
-            <p>{formatDate(data.created)}</p>
+            <p class="">{formatDate(data.created)}</p>
         </div>
     </div>
 </content>
