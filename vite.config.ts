fe627a5cea03622a327e79383e411737145267a4
Sven <sven-sve@proton.me>
2024-02-23 22:14:25 +0100

    build(deps): update package.json, vite.config.ts and yarn.lock

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite';
 import { version } from './package.json';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	define: {
-		'import.meta.env.VITE_APP_VERSION': JSON.stringify(version),
-	},
+    plugins: [sveltekit()],
+    define: {
+        'import.meta.env.VITE_APP_VERSION': JSON.stringify(version),
+    }
 });
