e4493114d30b1f70faab0b98e79520d3d388127c
Sven <sven-sve@proton.me>
2024-02-24 17:37:33 +0100

    update SelectApp.svelte > First project 🎊

--- src/lib/components/sidebar/SelectApp.svelte
+++ src/lib/components/sidebar/SelectApp.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+    <script src="https://cdn.jsdelivr.net/npm/@tsparticles/confetti@3.0.3/tsparticles.confetti.bundle.min.js"></script>
+</svelte:head>
+
 <script>
     import * as Tooltip from "$lib/components/ui/tooltip";
     import * as Dialog from "$lib/components/ui/dialog";
@@ -38,6 +42,41 @@
             toast.success(`${data.get('name')} has been created!`);
 
             document.dispatchEvent(new CustomEvent('REFRESH_PROJECTS'));
+
+            if(!records.length > 0) {
+                toast.info('Congrats; You just created your first project!');
+
+                const duration = 3000,
+                animationEnd = Date.now() + duration,
+                defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+                function randomInRange(min, max) {
+                return Math.random() * (max - min) + min;
+                }
+
+                const interval = setInterval(function() {
+                const timeLeft = animationEnd - Date.now();
+
+                if (timeLeft <= 0) {
+                    return clearInterval(interval);
+                }
+
+                const particleCount = 50 * (timeLeft / duration);
+
+                confetti(
+                    Object.assign({}, defaults, {
+                    particleCount,
+                    origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+                    })
+                );
+                confetti(
+                    Object.assign({}, defaults, {
+                    particleCount,
+                    origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+                    })
+                );
+                }, 250);
+            }
         } catch (e) {
             toast.error('Some error occurred while creating your project!')
         }
