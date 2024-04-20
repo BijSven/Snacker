253b914ed739fdfd6df18676a09c55c99f5359b9
Sven <sven-sve@proton.me>
2024-04-20 14:15:31 +0200

    update SelectApp.svelte > Quality Improvements

--- src/lib/components/sidebar/SelectApp.svelte
+++ src/lib/components/sidebar/SelectApp.svelte
@@ -51,30 +51,30 @@
                 defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
                 function randomInRange(min, max) {
-                return Math.random() * (max - min) + min;
+                    return Math.random() * (max - min) + min;
                 }
 
                 const interval = setInterval(function() {
-                const timeLeft = animationEnd - Date.now();
+                    const timeLeft = animationEnd - Date.now();
 
-                if (timeLeft <= 0) {
-                    return clearInterval(interval);
-                }
+                    if (timeLeft <= 0) {
+                        return clearInterval(interval);
+                    }
 
-                const particleCount = 50 * (timeLeft / duration);
+                    const particleCount = 50 * (timeLeft / duration);
 
-                confetti(
-                    Object.assign({}, defaults, {
-                    particleCount,
-                    origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
-                    })
-                );
-                confetti(
-                    Object.assign({}, defaults, {
-                    particleCount,
-                    origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
-                    })
-                );
+                    confetti(
+                        Object.assign({}, defaults, {
+                            particleCount,
+                            origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+                        })
+                    );
+                    confetti(
+                        Object.assign({}, defaults, {
+                            particleCount,
+                            origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+                        })
+                    );
                 }, 250);
             }
         } catch (e) {
@@ -190,7 +190,7 @@
                     <Switch bind:checked={modernMode} on:click={() => {
                         toast.warning('Unapplied changes', {
                             duration: Number.POSITIVE_INFINITY,
-                            description: `To apply your changes, refresh.`,
+                            description: `To apply your changes, refresh the page.`,
                             action: {
                                 label: 'Refresh',
                                 onClick: async () => {
